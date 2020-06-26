c235f702da65293ad8ef41597ab786090f861515
kappaggaeru <oliveralautaro99@gmail.com>
2020-06-26 00:04:25 -0300

    agregados archivos en la carpeta sql

--- sql/G8_Cambios.sql
+++ sql/G8_Cambios.sql
@@ -1,36 +1,41 @@
---Solo los usuarios que han jugado tendrán la opción de votar y recomendar un juego en particular.
---ambito general
-create assertion chk_JUEGO_FINALIZADO
-check not exists(
-    select 1
-    from GR8_RECOMENDACION r
-    join GR8_JUEGA j on r.id_usuario = j.id_usuario
-    join GR8_VOTO v on r.id_usuario = v.id_usuario
-    where finalizado = false;
-)
-
-CREATE TRIGGER tg_JUEGO_FINALIZADO_RECOMENDACION
-BEFORE INSERT OR UPDATE OF id_usuario,id_juego
-ON GR8_RECOMENDACION
-FOR EACH ROW
-EXECUTE PROCEDURE fn_JUEGO_FINALIZADO();
-
-CREATE TRIGGER tg_JUEGO_FINALIZADO_VOTO
-BEFORE INSERT OR UPDATE OF id_usuario,id_juego
-ON GR8_RECOMENDACION
-FOR EACH ROW
-EXECUTE PROCEDURE fn_JUEGO_FINALIZADO();
-
+-- Solo los usuarios que han jugado tendrán la opción de votar y recomendar un juego en particular.
 CREATE OR REPLACE FUNCTION fn_JUEGO_FINALIZADO() RETURNS Trigger AS $$
-DECLARE cant integer;
 BEGIN
+  IF(EXISTS(
     SELECT 1
     FROM GR8_JUEGA j
-    JOIN GR8_VOTO v ON v.id_usuario,v.id_juego = j.id_usuario,j.id_juego
-    JOIN GR8_RECOMENDACION r ON r.id_usuario,r.id_juego = j.id_usuario,j.id_juego
-    IF (finalizado = false) THEN
-      RAISE EXCEPTION 'Para votar/recomendar un juego debe haberlo finalizado', NEW.id_usuario,NEW.id_juego
-    END IF;
-    RETURN NEW;
+    WHERE (j.id_usuario = NEW.id_usuario AND j.id_juego = NEW.id_juego)
+    AND finalizado = false
+  ))
+  THEN RAISE EXCEPTION 'Para recomendar/votar un juego primero debe haberlo finalizado';
+  END IF;
+  RETURN NEW;
 END $$
-LANGUAGE 'plpgsql'
+LANGUAGE 'plpgsql';
+
+CREATE TRIGGER tg_RECOMENDACION_VALIDA
+BEFORE INSERT OR UPDATE OF id_usuario,id_juego
+ON GR8_RECOMENDACION
+FOR EACH ROW
+EXECUTE PROCEDURE fn_JUEGO_FINALIZADO();
+
+CREATE TRIGGER tg_VOTO_VALIDO
+BEFORE INSERT OR UPDATE OF id_usuario,id_juego
+ON GR8_VOTO
+FOR EACH ROW
+EXECUTE PROCEDURE fn_JUEGO_FINALIZADO();
+
+--**************************
+--Sentencia que activa la restricción:
+--INSERT INTO GR8_RECOMENDACION VALUES(1,'example@org.com',1,1);
+--Respuesta del DBMS:
+--ERROR:  Para recomendar/votar un juego primero debe haberlo finalizado
+--Se devuelve el mensaje de la excepción ya que esta cumplió su condición de activación
+--**************************
+
+-- La fecha del primer comentario tiene que ser anterior a la fecha del último comentario si este no es nulo.
+
+
+-- Cada usuario sólo puede comentar una vez al día cada juego.
+-- Un usuario no puede recomendar un juego si no ha votado previamente dicho juego.
+-- Un usuario no puede comentar un juego que no ha jugado.
